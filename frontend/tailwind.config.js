186d1b564fd433a6d1a52ffb34b644ee968a3a59
Amrita kumari mishra <amritamishraxa06@gmail.com>
2025-10-23 20:31:21 +0000

    feat(navbar): Implement responsive sidebar and fix Tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-  ],
+ content: [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+],
   prefix: "",
   theme: {
   	container: {
